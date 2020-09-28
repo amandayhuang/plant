3c004c9887ec193662eabfbf50b1eeed24b0eb0c
amanda <amandayhuang@gmail.com>
2020-09-28 19:19:49 -0400

    Updates

---
+++ precache-manifest.7dddf10a24454f2b2cc235d22d000b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e6f9c685e5dddec2f4042533473e70",
+    "url": "/plant/index.html"
+  },
+  {
+    "revision": "76744e703c776756d180",
+    "url": "/plant/static/css/main.1140e7a9.chunk.css"
+  },
+  {
+    "revision": "f1cb43f42183a8ac099d",
+    "url": "/plant/static/js/2.db126794.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/plant/static/js/2.db126794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76744e703c776756d180",
+    "url": "/plant/static/js/main.6bdc4249.chunk.js"
+  },
+  {
+    "revision": "2a232094208194465393",
+    "url": "/plant/static/js/runtime-main.fccad7e1.js"
+  }
+]);
